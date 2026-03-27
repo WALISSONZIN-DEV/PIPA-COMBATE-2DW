a8a31ec775bfb351e40df129284d4e9a33bb7d8e
WALISSONZIN-DEV <cortihugahuga@gmail.com>
2026-03-27 20:54:51 -0300

    Create constants.ts

---
+++ constants.ts
@@ -0,0 +1,23 @@
+export const KITE_COLORS = [
+  { body: '#e53e3e', tail: '#fc8181' },
+  { body: '#3182ce', tail: '#63b3ed' },
+  { body: '#38a169', tail: '#68d391' },
+  { body: '#ecc94b', tail: '#faf089' },
+  { body: '#805ad5', tail: '#b794f4' },
+  { body: '#dd6b20', tail: '#fbd38d' },
+  { body: '#e53e8c', tail: '#f687b3' },
+  { body: '#00b5d8', tail: '#76e4f7' },
+];
+
+export const PLAYER_COLOR = { body: '#e53e3e', tail: '#fc8181' };
+
+export const NUM_ENEMIES = 5;
+export const NUM_CLOUDS = 8;
+export const KITE_SIZE = 22;
+export const LINE_ANCHOR_Y_RATIO = 1.15;
+export const PLAYER_SPEED = 4;
+export const AI_SPEED_MIN = 1.5;
+export const AI_SPEED_MAX = 3;
+export const CUT_DISTANCE = 30;
+export const INITIAL_LINE_LENGTH = 200;
+
